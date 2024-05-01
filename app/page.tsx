952457e33c118db9368bd262d211f3a699868e98
alexandra002dev <alexandramarkess@gmail.com>
2024-05-01 09:44:06 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Header from "./_components/header";
+import SearchInput from "./_components/search-input";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <SearchInput />
+      </div>
+    </>
+  );
 };
 export default Home;
