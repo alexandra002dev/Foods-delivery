cb1f77f537b251c4b5c1d76c1a496a084fab20ca
alexandra002dev <alexandramarkess@gmail.com>
2024-05-10 14:44:31 -0300

    fix: add functions add, remove products cart

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -6,79 +6,72 @@ import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
 
 const Cart = () => {
-  const { products } = useContext(CartProductContext);
-  //*SUBTOTAL
-  const subtotal = products.reduce(
-    (acc, qt) => acc + Number(qt.price) * qt.quantity,
-    0,
-  );
-  //*ENTREGA
-  const entrega = products.reduce(
-    (acc, qt) => acc + Number(qt.restaurant.deliveryFee) * qt.quantity,
-    0,
-  );
-  //*DESCONTO
-  const desconto = products.reduce(
-    (acc, qt) => acc + Number(qt.discountPercentage) * qt.quantity,
-    0,
-  );
-  //*TOTAL
-
-  const total = subtotal - desconto + entrega;
+  const { products, subtotalPrice, totalPrice, totalDiscount } =
+    useContext(CartProductContext);
 
   return (
-    <div className="flex h-full flex-col justify-between">
-      <div>
-        <h2 className="text-lg font-bold"> Sacola</h2>
-        {products.map((item) => (
-          <CartItem productCart={item} key={item.id} />
-        ))}
-      </div>
-      <div>
-        <Card>
-          <CardContent>
-            {/* SUBTOTAL */}
-            <div className="flex justify-between py-3">
-              <h3>Subtotal</h3>
-              <span>R${formatCurrency(Number(subtotal))}</span>
-            </div>
-            <hr />
-            {/* ENTREGA */}
-            <div className="flex justify-between py-3">
-              <h3>Entrega</h3>
-              {Number(entrega) !== 0 ? (
-                <span
-                  className={`${Number(entrega) !== 0 ? "text-muted-foreground" : "text-red-600"}`}
-                >
-                  {" "}
-                  R${formatCurrency(Number(entrega))}
-                </span>
-              ) : (
-                <span
-                  className={`${Number(entrega) !== 0 ? "text-muted-foreground" : "text-red-600"}`}
-                >
-                  {" "}
-                  Grátis
-                </span>
-              )}
-            </div>
-            <hr />
-            {/* DESCONTO */}
-            <div className="flex justify-between py-3">
-              <h3>Desconto</h3>
-              <span>- R$ {formatCurrency(Number(desconto))}</span>
-            </div>
-            <hr />
-            {/* TOTAL */}
-            <div className="flex justify-between py-3 font-bold">
-              <h3>Total</h3>
-              <span>R$ {formatCurrency(Number(total))}</span>
-            </div>
-          </CardContent>
-        </Card>
-        <Button className="mt-5 w-full">Finalizar Pedido</Button>
-      </div>
-    </div>
+    <>
+      <h2 className="mb-2 text-lg font-bold"> Sacola</h2>
+      {products.length > 0 ? (
+        <div className="flex h-full flex-col justify-between">
+          <div>
+            {products.map((product) => (
+              <CartItem productCart={product} key={product.id} />
+            ))}
+          </div>
+          <div>
+            <Card>
+              <CardContent>
+                {/* SUBTOTAL */}
+                <div className="flex justify-between py-3">
+                  <h3>Subtotal</h3>
+                  <span>{formatCurrency(Number(subtotalPrice))}</span>
+                </div>
+                <hr />
+                {/* ENTREGA */}
+                <div className="flex justify-between py-3">
+                  <h3>Entrega</h3>
+
+                  {Number(products[0].restaurant.deliveryFee) === 0 ? (
+                    <span className="text-primary"> Grátis </span>
+                  ) : (
+                    <span>
+                      {formatCurrency(
+                        Number(products[0].restaurant.deliveryFee),
+                      )}
+                    </span>
+                  )}
+                </div>
+                <hr />
+                {/* DESCONTO */}
+                <div className="flex justify-between py-3">
+                  <h3>Desconto</h3>
+                  <span>- {formatCurrency(Number(totalDiscount))}</span>
+                </div>
+                <hr />
+                {/* TOTAL */}
+                <div className="flex justify-between py-3 font-bold">
+                  <h3>Total</h3>
+                  <span>
+                    {formatCurrency(
+                      Number(totalPrice) +
+                        Number(products[0].restaurant.deliveryFee),
+                    )}
+                  </span>
+                </div>
+              </CardContent>
+            </Card>
+            <Button className="mt-5 w-full">Finalizar Pedido</Button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex h-32 items-center justify-center text-center">
+          <span className="text-lg font-semibold text-muted-foreground">
+            Sacola Vazia!
+          </span>
+        </div>
+      )}
+    </>
   );
 };
 
