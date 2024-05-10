7fa0d7e361cc0e36e289c69e8ed18edc211a8ea9
alexandra002dev <alexandramarkess@gmail.com>
2024-05-10 20:12:48 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,21 +1,129 @@
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import {
+  HeartIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScrollTextIcon,
+} from "lucide-react";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data } = useSession();
+  const handleSignOutClick = () => signOut();
+  const handleSignInClick = () => signIn();
   return (
     <div className="flex justify-between px-5 pt-6">
       <Link href={"/"}>
         <Image src="/Logo.png" alt="FSW Foods" height={30} width={100} />
       </Link>
-      <Button
-        size="icon"
-        variant="outline"
-        className="bg-transparente border-none"
-      >
-        <MenuIcon />
-      </Button>
+
+      <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+        <SheetTrigger>
+          <Button
+            size="icon"
+            variant="outline"
+            className="bg-transparente border-none"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="w-[90vw] space-y-3">
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+          {data?.user ? (
+            <>
+              <div className="flex items-center">
+                {/* IMAGEM */}
+                <div className="relative h-12 w-12 rounded-full">
+                  <Avatar>
+                    <AvatarImage src={data.user?.image as string | undefined} />
+                    <AvatarFallback>
+                      {data.user?.name?.split(" ")[0][0]}
+                      {data.user?.name?.split(" ")[1][0]}
+                    </AvatarFallback>
+                  </Avatar>
+                </div>
+                {/* INFO */}
+                <div>
+                  <h2 className="text-lg font-semibold">{data.user?.name}</h2>
+                  <span className="text-sm">{data.user?.email}</span>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between text-lg font-semibold">
+                <h2>Olá. Faça seu login!</h2>
+                <Button size="icon" onClick={handleSignInClick}>
+                  <LogInIcon />
+                </Button>
+              </div>
+            </>
+          )}
+          <div className=" py-6 ">
+            <Separator />
+          </div>
+          <Button
+            variant="ghost"
+            className="w-full justify-start gap-3 rounded-full "
+          >
+            <HomeIcon size={16} /> Início
+          </Button>
+          {data?.user && (
+            <>
+              <Button
+                variant="ghost"
+                className="w-full justify-start gap-3 rounded-full "
+              >
+                <ScrollTextIcon size={16} /> Meus Pedidos
+              </Button>
+              <Button
+                variant="ghost"
+                className="w-full justify-start gap-3 rounded-full "
+              >
+                <HeartIcon size={16} /> Restaurantes Favoritos
+              </Button>
+            </>
+          )}
+
+          <div className=" py-6 ">
+            <Separator />
+          </div>
+          <Button variant="ghost" className="w-full justify-start gap-3 ">
+            <HeartIcon size={16} /> Pratos
+          </Button>
+          <div className=" py-6 ">
+            <Separator />
+          </div>
+          {data?.user && (
+            <Button
+              className="w-full justify-start gap-3 rounded-full"
+              onClick={handleSignOutClick}
+            >
+              <LogOutIcon size={16} />
+              Sair da conta
+            </Button>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
