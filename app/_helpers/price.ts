9df6b41c8850c98ab4e24e0b438cadaab2e9c598
alexandra002dev <alexandramarkess@gmail.com>
2024-05-10 14:46:50 -0300

    fix: add R$ function formatCurrency

--- app/_helpers/price.ts
+++ app/_helpers/price.ts
@@ -1,7 +1,7 @@
 import { Product } from "@prisma/client";
 
 export const calculateProductTotalPrice = (product: Product): number => {
-  if (product.discountPercentage == 0) {
+  if (product.discountPercentage === 0) {
     return Number(product.price);
   }
   const discount = Number(product.price) * (product.discountPercentage / 100);
@@ -9,8 +9,8 @@ export const calculateProductTotalPrice = (product: Product): number => {
 };
 
 export const formatCurrency = (value: number): string => {
-  return Intl.NumberFormat("pt-BR", {
-    minimumFractionDigits: 2,
+  return `R$${Intl.NumberFormat("pt-BR", {
     currency: "BRL",
-  }).format(value);
+    minimumFractionDigits: 2,
+  }).format(value)}`;
 };
