4c22728ee304b2cda02c1b24c7573b0d97a69f51
alexandra002dev <alexandramarkess@gmail.com>
2024-05-10 14:53:29 -0300

    fix: change db

--- app/product/recommended/page.tsx
+++ app/product/recommended/page.tsx
@@ -1,28 +1,38 @@
 import Header from "@/app/_components/header";
 import ProductItem from "@/app/_components/product-item";
-
 import { db } from "@/app/_lib/prisma";
-const RecommendedProducts = async () => {
-  const products = await db.product.findMany({
-    include: {
-      restaurant: true,
-    },
 
-    take: 10,
+const RecommendedProductsPage = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
   });
 
+  // TODO: pegar produtos com mais pedidos
   return (
-    <div className="px-5 ">
+    <>
       <Header />
-      <h2 className="mt-2 text-lg font-semibold ">Pedidos Recomendados</h2>
-
-      {products.map((products) => (
-        <div key={products.id} className="mt-3">
-          <ProductItem product={products} />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem key={product.id} product={product} />
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
-export default RecommendedProducts;
+export default RecommendedProductsPage;
